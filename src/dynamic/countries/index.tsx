e104f07803d5d53b2269c9b19a0429a71d9b6859
abhishek <abbhishekk27@gmail.com>
2023-03-21 20:36:56 +0530

    fix

--- src/dynamic/countries/index.tsx
+++ src/dynamic/countries/index.tsx
@@ -2,6 +2,6 @@ import India from './india';
 import Vietnam from './vietnam';
 import Turkey from './turkey';
 import Germany from './germany';
-import Mexico from './Mexico';
+import Mexico from './mexico';
 
 export { India, Vietnam, Turkey, Germany, Mexico };
